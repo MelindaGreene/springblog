caf7c76d4f97a3ac1e6feb75a5eb13357f3e7d78
MelindaGreene <Melindagreene711@gmail.com>
2020-01-27 12:13:47 -0600

    learning Spring

---
+++ src/main/java/com/codeup/springblog/Controllers/DeimosController.java
@@ -0,0 +1,17 @@
+package com.codeup.springblog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DeimosController {
+        @GetMapping("/deimos/{days}")
+        @ResponseBody
+
+        public String deimos(@PathVariable String days){
+            return "There are " + days + days + "til  Developer Days";
+        }
+    }
+
